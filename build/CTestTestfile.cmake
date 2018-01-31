333178c998b8d3a5ee3cfa03fb58d29204b5d22e
turtlebot <rinsurbs@gmail.com>
2018-01-31 16:01:08 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,21 @@
+# CMake generated Testfile for 
+# Source directory: /home/turtlebot/turtlebot_ws/src
+# Build directory: /home/turtlebot/turtlebot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(ros_astra_launch)
+SUBDIRS(turtlebot/turtlebot)
+SUBDIRS(turtlebot_apps/turtlebot_apps)
+SUBDIRS(turtlebot/turtlebot_bringup)
+SUBDIRS(turtlebot/turtlebot_capabilities)
+SUBDIRS(turtlebot_apps/turtlebot_rapps)
+SUBDIRS(cmvision)
+SUBDIRS(ros_astra_camera)
+SUBDIRS(turtlebot_apps/turtlebot_actions)
+SUBDIRS(turtlebot_apps/turtlebot_calibration)
+SUBDIRS(turtlebot_apps/turtlebot_follower)
+SUBDIRS(turtlebot_apps/turtlebot_navigation)
+SUBDIRS(turtlebot/turtlebot_teleop)
+SUBDIRS(turtlebot/turtlebot_description)
---
+++ build/cmvision/catkin_generated/cmvision-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(cmvision_MESSAGE_FILES "/home/turtlebot/turtlebot_ws/src/cmvision/msg/Blob.msg;/home/turtlebot/turtlebot_ws/src/cmvision/msg/Blobs.msg")
+set(cmvision_SERVICE_FILES "")
